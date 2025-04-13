e1367933e036c054429968f0351fa767414114b6
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-04-13 11:50:29 +0100

    Added the home and header file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import reactlogo from './assets/react.svg'
+import vitelogo from '/vite.svg'
 import './App.css'
+import Home from './Screen/Home'
 
 function App() {
+
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-    </>
+    <div>
+      <Home />
+    </div>
   )
 }
 
