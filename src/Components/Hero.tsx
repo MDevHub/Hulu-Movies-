f9561cbafd16a51a9eb73c75b45b2e30a85e18ae
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-04-13 17:13:05 +0100

    Got the movie List from Api

---
+++ src/Components/Hero.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../Services/GlobalApi'
+
+function Hero() {
+
+   const [movieList, setMovieList]=useState([])
+   useEffect(()=> {
+      getPopularMovies();
+   }, [])
+
+   const getPopularMovies = ()=>{
+      GlobalApi.getPopularMovies.then((resp:any)=>{
+         const result = resp.data.results;
+         setMovieList(result)
+         console.log(result);
+      })
+   }
+
+   return (
+      <div>
+         Hero
+      </div>
+   )
+}
+
+export default Hero
