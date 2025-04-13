c868eabc54ab141a513ed35b5abba25876e87237
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-04-13 18:14:18 +0100

    Designed the hero section (First Header Image)

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -3,7 +3,8 @@ import GlobalApi from '../Services/GlobalApi'
 
 function Hero() {
 
-   const [movieList, setMovieList]=useState([])
+   const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
+   const [movieList, setMovieList]=useState<any>([])
    useEffect(()=> {
       getPopularMovies();
    }, [])
@@ -11,14 +12,28 @@ function Hero() {
    const getPopularMovies = ()=>{
       GlobalApi.getPopularMovies.then((resp:any)=>{
          const result = resp.data.results;
-         setMovieList(result)
-         console.log(result);
+         setMovieList(result[4])
       })
    }
 
    return (
       <div>
-         Hero
+          <div className='absolute h-[85vh] bg-gradient-to-t   from-[#1e2126]
+            via-transparent to-transparent w-full'></div>
+         <div className='absolute mt-[350px] md:mt-[350px] px-10 md:px-24'>
+            <h2 className='text-[19px] lg:text-[27px]'>Watch only on HULU</h2>
+            <h2 className='text-[36px] lg:text-[47px] font-extrabold'>{movieList.original_title}</h2>
+            <div className='flex gap-5 mt-5'>
+               <button className='bg-white text-black rounded-sm'>PLAY</button>
+               <button className='bg-transparent border-2 border-white
+                  cursor-pointer rounded-sm hover:border-gray-500 '>
+                  DETAILS
+               </button>
+            </div>
+         </div>
+         <img src={IMAGE_BASE_URL+movieList.backdrop_path}
+         width={1920} height={1080}
+         className='h-[85vh] object-cover'/>
       </div>
    )
 }
